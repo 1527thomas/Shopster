89bb7070dec25a3b8f9537a040baa089cc1bfbf3
Thomas Wang <1527thomas@gmail.com>
2021-10-05 12:18:03 -0700

    Added an addtocart method prop thats passed down to Product, modified Navbar cart badge to be dynamic

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Products, Navbar } from './components'
 
 const App = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({})
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -12,16 +13,27 @@ const App = () => {
         setProducts(data);
     }
 
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart)
+    }
+
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     }, []);
 
-    
+    console.log(cart)
 
     return (
         <div>
-            <Navbar />
-            <Products products={products} />
+            <Navbar totalItems={cart.total_items}/>
+            <Products products={products} onAddToCart={handleAddToCart}/>
         </div>
     )
 }
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -4,10 +4,9 @@ import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
     const classes = useStyles();
 
-    console.log(product)
     return (
         <Card className={classes.root}>
             <CardMedia className={classes.media} image={product.image.url} title={product.name} />
@@ -24,7 +23,7 @@ const Product = ({ product }) => {
             </CardContent>
 
             <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label="Add to Cart">
+                <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
                     <AddShoppingCart />
                 </IconButton>
             </CardActions>
