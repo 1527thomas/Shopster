e3a606e0fe656e179056d0df2d8f31dfad51fead
Thomas Wang <1527thomas@gmail.com>
2021-10-05 13:16:45 -0700

    Implemented routing between cart and home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
+import { Link, useLocation } from 'react-router-dom'
 
 import logo from '../../assets/Shopster.png'
 import useStyles from './styles'
 
 const Navbar = ({ totalItems }) => {
     const classes = useStyles();
+    const location = useLocation();
+
+
     return (
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Shopster" height="25px" className={classes.image} />
                         Shopster
                     </Typography>
                     <div className={classes.grow} />
-                    <div className={classes.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
-                            <Badge badgeContent={totalItems} color="secondary">
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
-                    </div>
+                    {location.pathname === '/' && (
+                        <div className={classes.button}>
+                            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                                <Badge badgeContent={totalItems} color="secondary">
+                                    <ShoppingCart />
+                                </Badge>
+                            </IconButton>
+                        </div>)}
                 </Toolbar>
             </AppBar>
         </>
