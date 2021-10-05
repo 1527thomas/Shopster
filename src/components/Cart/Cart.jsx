76010b90a863d24d494b9e341411f8ab5d715814
Thomas Wang <1527thomas@gmail.com>
2021-10-05 13:16:45 -0700

    Implemented routing between cart and home

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom'
 
 import useStyles from './styles'
 import CartItem from './CartItem/CartItem'
@@ -9,7 +10,9 @@ const Cart = ({ cart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
-        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+        <Typography variant="subtitle1">You have no items in your shopping cart,
+            <Link to="/" className={classes.link}> start adding some</Link>
+        </Typography>
     );
 
     const FilledCart = () => (
