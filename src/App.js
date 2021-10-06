4b4461e1f420755b1a1fab874a666aa1642d6373
Thomas Wang <1527thomas@gmail.com>
2021-10-05 21:38:24 -0700

    Checkout and address form implemented

--- src/App.js
+++ src/App.js
@@ -1,75 +1,75 @@
-import React, { useState, useEffect } from 'react'
-import { commerce } from './lib/commerce';
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
 
-import { Products, Navbar, Cart } from './components'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Products, Navbar, Cart, Checkout } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState({})
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
-    const fetchProducts = async () => {
-        const { data } = await commerce.products.list();
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
 
-        setProducts(data);
-    }
+    setProducts(data);
+  };
 
-    const fetchCart = async () => {
-        setCart(await commerce.cart.retrieve());
-    }
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
 
-    const handleAddToCart = async (productId, quantity) => {
-        const { cart } = await commerce.cart.add(productId, quantity);
+  const handleAddToCart = async (productId, quantity) => {
+    const { cart } = await commerce.cart.add(productId, quantity);
 
-        setCart(cart)
-    }
+    setCart(cart);
+  };
 
-    const handleUpdateCartQty = async (productId, quantity) => {
-        const { cart} = await commerce.cart.update(productId, { quantity })
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
 
-        setCart(cart)
-    }
+    setCart(cart);
+  };
 
-    const handleRemoveFromCart = async (productId) => {
-        const { cart } = await commerce.cart.remove(productId)
-        
-        setCart(cart)
-    }
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
 
-    const handleEmptyCart = async () => {
-        const { cart } = await commerce.cart.empty();
+    setCart(cart);
+  };
 
-        setCart(cart)
-    }
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
 
-    useEffect(() => {
-        fetchProducts();
-        fetchCart();
-    }, []);
+    setCart(cart);
+  };
 
-    console.log(cart)
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
 
-    return (
-        <Router>
-            <div>
-                <Navbar totalItems={cart.total_items}/>
-                <Switch>
-                    <Route exact path="/">
-                        <Products products={products} onAddToCart={handleAddToCart}/>
-                    </Route>
-                    <Route exact path="/cart">
-                        <Cart 
-                        cart={cart} 
-                        handleUpdateCartQty={handleUpdateCartQty}
-                        handleRemoveFromCart={handleRemoveFromCart}
-                        handleEmptyCart={handleEmptyCart}
-                        />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-        
-    )
-}
+  return (
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart
+              cart={cart}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+              handleEmptyCart={handleEmptyCart}
+            />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
